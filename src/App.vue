2db0db4bc49e421b6495424956242affcbe9fd1b
SecretDevStar <ztoc0130@gmail.com>
2023-03-03 01:16:11 +0900

    form component ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup>
 import InfoForm from "./components/InfoForm.vue";
+import MapView from "./components/MapView.vue";
+
+const handleSubmit = (e) => {
+  console.log(e)
+}
+
 </script>
 
 <template>
-  <InfoForm />
+  <div class="container">
+    <h1 class="my-4 text-center">Test</h1>
+    <div class="row">
+      <div class="col-4">
+        <InfoForm />
+      </div>
+      <div class="col-8">
+        <MapView></MapView>
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/InfoForm.vue
+++ src/components/InfoForm.vue
@@ -1,9 +1,44 @@
+<script setup>
+import {ref} from 'vue'
+
+const name = ref("")
+const description = ref("")
+const latitude = ref("")
+const longitude = ref("")
+
+const isEmpty = () => {
+  if(name.value==="" || description.value==="" || latitude.value==="" || longitude.value===""){
+    return true;
+  }
+  return false;
+}
+
+const handleSubmit = () => {
+  alert("Dd")
+}
+
+</script>
+
 <template>
-  <form>
-    <input type="text" placeholder="name">
-    <input type="text" placeholder="description">
-    <input type="number" placeholder="">
-    <input type="number" placeholder="">
-    <button>Submit</button>
-  </form>
+  <div class="info-form p-4">
+    <form @submit.prevent="handleSubmit">
+      <div class="form-item">
+        <label for="">Location Name:</label>
+        <input class="form-control" type="text" v-model="name" placeholder="name" />
+      </div>
+      <div class="form-item">
+        <label for="">Description:</label>
+        <textarea class="form-control" name="" id="" v-model="description" cols="30" rows="10"></textarea>
+      </div>
+      <div class="form-item">
+        <label for="">Latitude: </label>
+        <input class="form-control" type="number" v-model="latitude" pattern="^\d*(\.\d{0,2})?$" step="0.01" placeholder="" />
+      </div>
+      <div class="form-item">
+        <label for="">Longitude: </label>
+        <input class="form-control" type="number" v-model="longitude" pattern="^\d*(\.\d{0,2})?$" step="0.01" placeholder="" />
+      </div>
+      <button class="btn btn-primary form-control" :disabled="isEmpty()">Submit</button>
+    </form>
+  </div>
 </template>
